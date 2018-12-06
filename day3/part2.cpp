277e0d952d650ef33119e0551ad75d2952ce8602
beyonddream <43626691+beyonddream@users.noreply.github.com>
2018-12-05 19:36:15 -0800

    add day3/part2

--- day3/part2.cpp
+++ day3/part2.cpp
@@ -5,13 +5,16 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <map>
+#include <set>
 
 using namespace std;
 
 int main() {
 
-    int overlapping_claims = 0;
     int fabric[1001][1001] = { {0} };
+    map<string, string> cellToClaimIdMap;
+    set<string> uniqueClaimIds;
 
     string currentClaim;
     while(getline(cin, currentClaim) && !currentClaim.empty()) {
@@ -19,31 +22,35 @@ int main() {
         stringstream ss(currentClaim);
 
         string ignore;
+        string claimId;
         string x_y;
         string width_height;
-        ss >> ignore >> ignore >> x_y >> width_height;
+
+        ss >> claimId >> ignore >> x_y >> width_height;
 
         int left = stoi(x_y.substr(0, x_y.find(",")));
         int top = stoi(x_y.substr(x_y.find(",")+1, x_y.size() - 1));
         int width = stoi(width_height.substr(0, width_height.find("x")));
         int height = stoi(width_height.substr(width_height.find("x") + 1, width_height.size()));
 
+        uniqueClaimIds.insert(claimId);
+
         for(int x = left; x < left + width; x++) {
             for (int y = top; y < top + height; y++) {
                 fabric[x][y]++;
+                string cell_index = to_string(x)+"_"+to_string(y);
+                if (cellToClaimIdMap.find(cell_index) !=  cellToClaimIdMap.end()) {
+                    uniqueClaimIds.erase(cellToClaimIdMap[cell_index]);
+                    uniqueClaimIds.erase(claimId);
+                }
+                cellToClaimIdMap[cell_index] = claimId;
             }
         }
     }
 
-    for(int x = 0; x < 1001; x++) {
-        for(int y = 0; y < 1001; y++) {
-            if(fabric[x][y] > 1) {
-                ++overlapping_claims;
-            }
-        }
-    }
+    assert(uniqueClaimIds.size() == 1);
 
-    cout << "number of square inches within two or more claims is " << overlapping_claims << endl;
+    cout << "ID of the only claim that doesn't overlap is " << *uniqueClaimIds.begin() << endl;
 
     return EXIT_SUCCESS;
 }
